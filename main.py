4563ad1f1426633b97eae6c3f5a273893cbe1887
Viktor <vikt.mag@gmail.com>
2022-07-07 23:21:58 +0200

    Added comments to the new config variables

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ delay = 250             #Delay between opening links in milliseconds
 defaultdir = DESKTOP    #Default directory when selecting file
 autoclose = False       #Closes program after opening links if True
 opentxtfile = False     #Opens text file in default text editor if True
-savetxt = False
-savedtxtpath = ""
+savetxt = False         #Automatically selects saved file on startup if True
+savedtxtpath = ""       #Filepath to the file to be automatically selected
 
 config = ConfigParser(default_section=None)         #Stops [DEFAULT] in config.ini from being overwritten
 has_config = pathlib.Path("config.ini").exists()
